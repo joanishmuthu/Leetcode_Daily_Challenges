8ccafff1318208c4023ad5e9e2c8a1162d427fea
joanishmuthu <mjoanish@gmail.com>
2023-04-16 09:17:12 +0530

    DSA

--- src/com/leetcode75/RemovingStars.java
+++ src/com/leetcode75/RemovingStars.java
@@ -2,13 +2,30 @@ package com.leetcode75;
 
 public class RemovingStars {
     public static String removeStars(String s) {
-    	 StringBuilder sb = new StringBuilder();
-    	    for (final char c : s.toCharArray())
-    	      if (c == '*')
-    	        sb.deleteCharAt(sb.length() - 1);
-    	      else
-    	        sb.append(c);
-    	    return sb.toString();
+        int i=0;
+    	while(i<s.length()) {
+        	if(s.charAt(i)=='*') {
+        		s=s.substring(0, i-1)+s.substring(i+1,s.length());
+        		i=0;
+        		String[] r=s.split("");
+        		boolean flag=false;
+        		for(int j=0;j<r.length;j++) {
+        			if(r[j].equals("*")) {
+        				flag=true;
+        			}
+        			
+        		}
+        		if(flag) {
+        			continue;
+        		}
+        		else {
+        			break;
+        		}
+        		
+        	}
+        	i++;
+        }
+    	return s;
     }
 	public static void main(String[] args) {
 		String s="leet**cod*e";
