409480225e3416ef4eb5a205ec7d5eaf9bcbdf5e
joanishmuthu <mjoanish@gmail.com>
2023-04-16 09:17:40 +0530

    DSA

--- src/com/leetcode75/LongestPalindrome.java
+++ src/com/leetcode75/LongestPalindrome.java
@@ -1,44 +1,25 @@
 package com.leetcode75;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map.Entry;
-
 public class LongestPalindrome {
+	static int p=0;
     public static int longestPalindrome(String s) {
-        String[] str=s.split("");
-        HashMap<String, Integer> k=new HashMap<>();
-        for(int i=0;i<str.length;i++) {
-        	int c=1;
-        	for(int j=i+1;j<str.length;j++) {
-        		if(str[i].equals(str[j])) {
-        			c++;
-        		}
-        	}
-        	if(!k.containsKey(str[i])) {
-        		k.put(str[i], c);
-        	}
-        }
-        List<Integer> uj=new ArrayList<Integer>(k.values());
-        Collections.sort(uj);
-        int u=1;
-        String res="";
-        for(int i=0;i<(uj.get(uj.size()-1));i++) {
-        	for(Entry<String, Integer> entry:k.entrySet()) {
-        		if(u==entry.getValue()) {
-        			res=res+entry.getKey()+res;
-        			u++;
-        			break;
-        		}
-        	}
-        }
-        System.out.println();
-        return 0;
+    	for(int i=0;i<s.length();i++) {
+    		PalinChecker(s.substring(0, i));
+    	}
+    	return p;
     }
-    public static void main(String[] args) {
-		String s="jooaanish";
-		longestPalindrome(s);
+    private static void PalinChecker(String substring) {
+		// TODO Auto-generated method stub
+    	String res="";
+		for(int i=substring.length()-1;i>=0;i--) {
+			res=res+substring.charAt(i);
+		}
+		if(res.equals(substring) && res.length()>p) {
+			p=res.length();
+		}
+	}
+	public static void main(String[] args) {
+		String s="bbbab";
+		System.out.println(longestPalindrome(s));
 	}
 }
